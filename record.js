1e50dbe27e84c294023ea2a279b5987e73be31a3
Solomon Osiberu <lokobos67@gmail.com>
2016-01-29 14:41:54 +0000

    Second test passed. A record has a price

--- record.js
+++ record.js
@@ -1,17 +1,19 @@
 
 
-var Record = function(title, artist, publisher, year){
+var Record = function(title, artist, price, publisher, year){
+  this.title = title;
   this.artist = artist;
+  this.price = price;
   this.publisher = publisher;
   this.year = year;
 }
 
 
 
-var Blackstar = new Record("Blackstar", "David Bowie", "Columbia", 2016);
-var TwentyFive = new Record("25", "Adele", "XL Recordings", 2015);
-var BestOfBowie= new Record("Best Of Bowie", "David Bowie", "EMI", 2002);
-var DeathOfABachelor = new Record("Death Of A Bachelor", "Panic! at the Disco", "ATLANTIC", 2016);
-var Purpose = new Record("Purpose", "Justin Bieber", "Virgin EMI", 2015);
+var blackstar = new Record("Blackstar", "David Bowie", 9.99, "Columbia", 2016);
+var twentyFive = new Record("25", "Adele", 12.99, "XL Recordings", 2015);
+var bestOfBowie= new Record("Best Of Bowie", "David Bowie", 7.99, "EMI", 2002);
+var deathOfABachelor = new Record("Death Of A Bachelor", "Panic! at the Disco", 8.99, "ATLANTIC", 2016);
+var purpose = new Record("Purpose", "Justin Bieber", 9.99, "Virgin EMI", 2015);
 
 module.exports = Record
--- record_store_spec.js
+++ record_store_spec.js
@@ -5,4 +5,9 @@ describe('Record', function(){
   it('A record should have an artist', function(){ purpose = new Record("Purpose", "Justin Bieber", "Virgin EMI", 2015);
     assert.equal('Justin Bieber', purpose.artist);
   });
+  it('A record should have a price', function(){ twentyFive = new Record("25", "Adele", 12.99, "XL Recordings", 2015);
+    assert.equal(12.99, twentyFive.price);
+  });
+
+
 });
